1050f10cd251319a7b376b85875af0451319c459
Alexander Gusev <goooseman@me.com>
2019-03-23 02:08:54 +0200

    feat: bin folder with first utils for mac, path var for code bin (VS Code)

---
+++ dot_dotfiles/setup/linux/inotify.sh
@@ -0,0 +1,2 @@
+# https://code.visualstudio.com/docs/setup/linux#_visual-studio-code-is-unable-to-watch-for-file-changes-in-this-large-workspace-error-enospc
+fs.inotify.max_user_watches=524288
---
+++ dot_dotfiles/source/path/osx/code.sh
@@ -0,0 +1,1 @@
+export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
