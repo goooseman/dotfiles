a228c328d11640cc9d1df49d0461c8ad8084acb2
Alexander Gusev <goooseman@me.com>
2019-03-23 22:35:42 +0200

    feat: ansible script for ubuntu linux; speedtest-cli bin; chezmoi config using ansible;

---
+++ dot_dotfiles/setup/executable_osx.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Install Homebrew, if not already installed
+which brew > /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install Ansible, if not already installed
+which ansible-playbook > /dev/null || brew install ansible
+
+# Provision machine with ansible
+
+if [ ! -d "~/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi 
+
+if [ -z "$1" ];
+then
+    : # $1 was not given
+    ansible-playbook -i "localhost," -c local --become-method=su osx.yml
+else
+    : # $1 was given
+    ansible-playbook -i "localhost," -c local --become-method=su osx.yml --start-at-task "$1"
+fi
