856b67176c238d5ecba25c7bca071ad4791b1ac5
Alexander Gusev <goooseman@me.com>
2019-03-24 07:36:40 +0200

    feat: docker install; android installation path;

--- dot_dotfiles/source/path/android.sh
+++ dot_dotfiles/source/path/android.sh
@@ -1,1 +1,4 @@
-export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH"
+export PATH="$ANDROID_HOME/emulator:$PATH"
+export PATH="$ANDROID_HOME/tools:$PATH"
+export PATH="$ANDROID_HOME/tools/bin:$PATH"
+export PATH="$ANDROID_HOME/platform-tools:$PATH"
