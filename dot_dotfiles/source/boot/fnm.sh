278792bc64df012fb80de48fc714d59b14f3bb4e
Alexander Gusev <goooseman@me.com>
2019-03-24 12:45:31 +0200

    chore: just a linebreak move;

--- dot_dotfiles/source/boot/fnm.sh
+++ dot_dotfiles/source/boot/fnm.sh
@@ -1,6 +1,6 @@
 eval "`$HOME/.fnm/fnm env --multi`"
-# Autoload fnm version, when cd to dir
 
+# Autoload fnm version, when cd to dir
 autoload -U add-zsh-hook
 load-fnm() {
   if [[ -f .nvmrc && -r .nvmrc ]]; then
