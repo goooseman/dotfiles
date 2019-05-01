5d7bcfe72435ab7409eb593be1d45fb5e33be402
Alexander Gusev <goooseman@me.com>
2019-05-01 18:45:17 +0300

    feat: raspberry pi setup;

---
+++ dot_dotfiles/setup/executable_pi.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+install_ansible () {
+    sudo apt-get update
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository --yes --update ppa:ansible/ansible
+    sudo apt-get install ansible
+}
+
+# Install Ansible, if not already installed
+which ansible-playbook > /dev/null || install_ansible
+
+# Install requirements
+
+# ansible-galaxy install -r $HOME/.dotfiles/setup/pi_requirements.yml
+
+# Get a sudo password in the current shell
+
+sudo echo "I'm a superuser"
+
+# Provision machine with ansible
+
+if [ -z "$1" ];
+then
+    : # $1 was not given
+    ansible-playbook -i "localhost," -c local --become-method=sudo $HOME/.dotfiles/setup/pi.yml
+else
+    : # $1 was given
+    ansible-playbook -i "localhost," -c local --become-method=sudo $HOME/.dotfiles/setup/pi.yml --start-at-task "$1"
+fi
