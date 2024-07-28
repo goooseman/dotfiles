8656891dfc8979baa32e683695846433cf22547e
Alexander Gusman <alexander_gusman@intuit.com>
2024-07-28 21:55:43 +0300

    feat(oh-my-zsh): chezmoi support

--- dot_dotfiles/source/aliases/oh-my-zsh.sh
+++ dot_dotfiles/source/aliases/oh-my-zsh.sh
@@ -1,3 +1,3 @@
 alias zshconfig="EDITOR=code chezmoi edit ~/.zshrc"
-alias zshsource="source ~/.zshrc"
+alias zshsource="chezmoi -v apply && source ~/.zshrc"
 alias ohmyzsh="EDITOR=code chezmoi edit ~/.oh-my-zsh"
