a095a5c68a2d2f76d19611f157f8e79266bb0708
Alexander Gusman <alexander_gusman@intuit.com>
2024-07-28 22:51:42 +0300

    feat(zsh): profiler

--- dot_dotfiles/source/aliases/oh-my-zsh.sh
+++ dot_dotfiles/source/aliases/oh-my-zsh.sh
@@ -1,3 +1,4 @@
 alias zshconfig="EDITOR=code chezmoi edit ~/.zshrc"
 alias zshsource="chezmoi -v apply && source ~/.zshrc"
 alias ohmyzsh="EDITOR=code chezmoi edit ~/.oh-my-zsh"
+alias zshprofile="time ZSH_DEBUGRC=1 zsh -i -c exit"
