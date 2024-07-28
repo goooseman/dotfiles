b02f35e239dcc8f220ef3e5d9e9d24866a794f6c
Alexander Gusman <alexander_gusman@intuit.com>
2024-07-28 20:59:22 +0300

    fix: iterm tmux integration

--- dot_dotfiles/source/boot/osx/iterm2.sh
+++ dot_dotfiles/source/boot/osx/iterm2.sh
@@ -1,3 +1,5 @@
+# https://gitlab.com/gnachman/iterm2/-/wikis/tmux-Integration-Best-Practices#how-do-i-use-shell-integration
+export ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=YES
 if [ -n "$ZSH_VERSION" ]; then
     test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 fi
