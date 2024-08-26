b96de64f53f758d8ac57579e79f9125ea4518f0f
Alexander Gusman <alexander_gusman@intuit.com>
2024-08-26 11:26:03 +0300

    fix(nvim): system clipboard coms

--- dot_config/nvim/lua/goooseman/remap.lua
+++ dot_config/nvim/lua/goooseman/remap.lua
@@ -3,3 +3,6 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
+vim.keymap.set({ "n", "v" }, "<leader>P", [["+P]])
