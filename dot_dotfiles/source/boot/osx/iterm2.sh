83a1298b593263608e1fb447f90061805540a662
Alexander Gusman <alexander_gusman@intuit.com>
2026-09-22 17:43:59 -0700

    fix: remove iterm2

--- dot_dotfiles/source/boot/osx/iterm2.sh
+++
@@ -1,5 +0,0 @@
-# https://gitlab.com/gnachman/iterm2/-/wikis/tmux-Integration-Best-Practices#how-do-i-use-shell-integration
-export ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=YES
-if [ -n "$ZSH_VERSION" ]; then
-    test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-fi
